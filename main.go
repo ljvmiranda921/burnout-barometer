0a7ce82dbcfddcb31d4ea0ec07dcc005b409ba67
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-01-03 10:36:14 +0800

    Add 'cmd' package for CLI

---
+++ main.go
@@ -0,0 +1,15 @@
+package barometer
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ljvmiranda921/burnout-barometer/cmd"
+)
+
+func main() {
+	if err := cmd.NewCommand().Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
