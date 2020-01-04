d447e690576c8da69b3b80ac7f1a0411ce676e21
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-01-04 13:57:55 +0800

    Add InitCommand() to root

--- cmd/root.go
+++ cmd/root.go
@@ -30,6 +30,7 @@ func NewCommand() *cobra.Command {
 	command.PersistentFlags().CountVarP(&verbosity, "verbosity", "v", "set verbosity")
 
 	// Add subcommands
+	command.AddCommand(InitCommand())
 	command.AddCommand(ServeCommand())
 
 	return command
