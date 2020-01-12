18528d477ee132054ebd8566facc43c376269277
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-01-12 20:49:23 +0800

    [wip] Use gotests to setup go structure

--- pkg/barometer_test.go
+++ pkg/barometer_test.go
@@ -1,41 +1,54 @@
+// Copyright 2019 Lester James V. Miranda. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root
+// for license information.
+
 package pkg
 
 import (
-	"reflect"
-	"strconv"
 	"testing"
-	"time"
 )
 
 func TestRequest_ParseMessage(t *testing.T) {
-	type test struct {
-		input string
-		want  []string
+	type fields struct {
+		Text      string
+		UserID    string
+		Timestamp string
+		Area      string
+		DB        Database
+		Item      Log
 	}
-
-	tests := []test{
-		{input: "4 hello", want: []string{"4", "hello"}},
-		{input: "4 hello world", want: []string{"4", "hello world"}},
-		{input: "4", want: []string{"4", ""}},
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		want1   string
+		wantErr bool
+	}{
+		{name: "single notes", fields: fields{Text: "4 hello"}, want: "4", want1: "hello", wantErr: false},
+		{name: "multiple notes", fields: fields{Text: "4 hello world"}, want: "4", want1: "hello world", wantErr: false},
+		{name: "no notes", fields: fields{Text: "4"}, want: "4", want1: "", wantErr: false},
 	}
-
-	for _, tc := range tests {
-		request := &Request{Text: tc.input}
-		measure, notes, _ := request.ParseMessage()
-		got := []string{measure, notes}
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Errorf("expected: %v, got: %v", tc.want, got)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Request{
+				Text:      tt.fields.Text,
+				UserID:    tt.fields.UserID,
+				Timestamp: tt.fields.Timestamp,
+				Area:      tt.fields.Area,
+				DB:        tt.fields.DB,
+				Item:      tt.fields.Item,
+			}
+			got, got1, err := r.ParseMessage()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Request.ParseMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Request.ParseMessage() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("Request.ParseMessage() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
 	}
-}
-
-func TestRequest_GetTimestamp(t *testing.T) {
-	timeNow := strconv.FormatInt(time.Now().Unix(), 10)
-	t.Logf("time now in unix: %s", timeNow)
-	request := &Request{Timestamp: timeNow, Area: "Asia/Manila"}
-	ts, err := request.GetTimestamp()
-	if err != nil {
-		t.Errorf("error getting time: %v", err)
-	}
-	t.Logf("time now in human-readable format: %s", ts)
 }
