e91cfff5d4e3d6cd3e9ec6de3b6cd7c3a09d8363
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-01-13 20:04:29 +0800

    [wip] Add tests for parseMessage()

--- pkg/barometer_test.go
+++ pkg/barometer_test.go
@@ -50,3 +50,50 @@ func TestRequest_Process(t *testing.T) {
 		})
 	}
 }
+
+func TestRequest_parseMessage(t *testing.T) {
+	type fields struct {
+		Text      string
+		UserID    string
+		Timestamp string
+		Area      string
+		DB        Database
+		DebugOnly bool
+		item      logItem
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		want1   string
+		wantErr bool
+	}{
+		{name: "single notes", fields: fields{Text: "4 hello"}, want: "4", want1: "hello", wantErr: false},
+		{name: "multiple notes", fields: fields{Text: "4 hello world"}, want: "4", want1: "hello world", wantErr: false},
+		{name: "no notes", fields: fields{Text: "4"}, want: "4", want1: "", wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Request{
+				Text:      tt.fields.Text,
+				UserID:    tt.fields.UserID,
+				Timestamp: tt.fields.Timestamp,
+				Area:      tt.fields.Area,
+				DB:        tt.fields.DB,
+				DebugOnly: tt.fields.DebugOnly,
+				item:      tt.fields.item,
+			}
+			got, got1, err := r.parseMessage()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Request.parseMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Request.parseMessage() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("Request.parseMessage() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
