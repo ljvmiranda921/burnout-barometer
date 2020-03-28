0571c6a8f82be13c220b6848f24d2b606cb6a2e3
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-03-29 00:29:37 +0800

    Add unit test for non-int error-handler

--- pkg/barometer_test.go
+++ pkg/barometer_test.go
@@ -54,6 +54,12 @@ func TestRequest_Process(t *testing.T) {
 			want:    &Message{},
 			wantErr: true,
 		},
+		{
+			name:    "non-int measure",
+			fields:  fields{Text: "A hello world", DebugOnly: true, Timestamp: strconv.FormatInt(time.Now().Unix(), 10), Area: "Asia/Manila"},
+			want:    &Message{},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
